8875aa120196a99fe4341048c59d8a2bc977dc76
Sergio <anol1987@hotmail.com>
2021-04-12 20:01:00 -0400

    regresion logistica en python

--- Random_forest.R
+++ Random_forest.R
@@ -17,4 +17,13 @@ y_pred = predict(regression, newdata = data.frame(Level = 6.5))
 # install.packages("ggplot2")
 
 library(ggplot2)
-x_grid <- seq
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+ggplot() +
+  geom_point(aes(x = dataset$Level , y = dataset$Salary),
+             color = "red") +
+  geom_line(aes(x = x_grid, y = predict(regression, 
+                                        newdata = data.frame(Level = x_grid))),
+            color = "blue") +
+  ggtitle("Predicción (Random Forest)") +
+  xlab("Nivel del empleado") +
+  ylab("Sueldo (en $)")

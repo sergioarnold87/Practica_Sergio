73ff0b572eb9b49e66adf75e8283dd042cccedeb
Sergio <anol1987@hotmail.com>
2021-04-27 16:31:56 -0400

    Update login_scrapy_github.py

--- login_scrapy_github.py
+++ login_scrapy_github.py
@@ -19,3 +19,36 @@ from scrapy.loader import ItemLoader
 from bs4 import BeautifulSoup
 from scrapy.crawler import CrawlerProcess
 import scrapy
+
+class LoginSpider(Spider):
+  name = 'GitHubLogin'
+  start_urls = ['https://github.com/login']
+
+  def parse(self, response):
+    return scrapy.FormRequest.from_response(
+      response,
+      formdata={'login': 'sergioarnold87', 'password': open('./password.txt').readline().strip()},
+      callback=self.after_login
+    )
+
+
+  def after_login(self, response):
+    request = scrapy.Request(
+      'https://github.com/sergioarnold87?tab=repositories',
+      callback=self.parse_repositorios
+    )
+    yield request
+
+  def parse_repositorios(self, response):
+    sel = Selector(response);
+    repositorios = sel.xpath('//h3[@class="wb-break-all"]/a/text()')
+    for repositorio in repositorios:
+      print (repositorio.get())
+
+process = CrawlerProcess({
+    'FEED_FORMAT': 'json',
+    'FEED_URI': 'datos_de_salida.json'
+})
+process.crawl(LoginSpider)
+process.start()
+
